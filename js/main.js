5b1f1983c71e19e9ac0f567a1976ba63fa428985
lcb931023 <lcb931023@gmail.com>
2014-10-08 10:51:10 -0400

    Player separated as a class

--- js/main.js
+++ js/main.js
@@ -5,22 +5,18 @@ function init() {
 	var height = document.getElementById("game-canvas").height;
 	stage = new PIXI.Stage(0xFF00CC);
 	renderer = PIXI.autoDetectRenderer(
-	width,
-	height,
-	document.getElementById("game-canvas")
+		width,
+		height,
+		document.getElementById("game-canvas")
 	);
 
-	var playerPicture = PIXI.Texture.fromImage("pics/player.png");
-
-	player = new PIXI.Sprite(playerPicture, 100);
-	player.anchor.x = 0.5;
-	player.anchor.y = 0.5;
+	player = new Player();
 	player.position.x = width/2;
 	player.position.y = height/2;
 	stage.addChild(player);
 
 
-	/*** Render what was just instantiated ***/
+	/*** Start updating through draw loop ***/
 	draw();
 }
 
---
+++ js/player.js
@@ -0,0 +1,10 @@
+function Player() {
+	var texture = PIXI.Texture.fromImage("pics/player.png");
+	PIXI.Sprite.call(this, texture);
+	
+	this.anchor.x = 0.5;
+	this.anchor.y = 0.5;
+}
+
+Player.constructor = Player;
+Player.prototype = Object.create(PIXI.Sprite.prototype);
