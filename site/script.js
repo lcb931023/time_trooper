e07dac96dc0d88e4557862c8a1a1dfe16bc962be
unknown <mln6406@NM2550-15.main.ad.rit.edu>
2014-10-14 22:36:18 -0400

    Seriously Joking Site

---
+++ site/script.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+
+    $(document).foundation({});
+
+});
+
+function scrollToElement(selector, time, verticalOffset) {
+    time = typeof(time) != 'undefined' ? time : 1000;
+    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+    element = $(selector);
+    offset = element.offset();
+    offsetTop = offset.top + verticalOffset;
+    $('html, body').animate({
+        scrollTop: offsetTop
+    }, time);
+}
