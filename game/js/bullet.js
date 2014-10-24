959f177e279e74b6b639837e5dab7cbc7c6374b4
lcb931023 <lcb931023@gmail.com>
2014-10-24 09:49:22 -0400

    a starting point for bullets

--- game/js/bullet.js
+++ game/js/bullet.js
@@ -1,10 +1,14 @@
-function Bullet() {
+/* pDx, pDy: Bullet's intended movement directions
+ * pScrollX: Game Scrolling applied to Bullets
+ */
+function Bullet( pDx, pDy, pScrollX ) {
 	var texture = PIXI.Texture.fromImage("pics/bullet.png");
 	PIXI.Sprite.call(this, texture);
 	
 	this.anchor.x = 0.5;
 	this.anchor.y = 0.5;
-	this.dx = - (Math.random() * .2 + .4);
+	this.dx = pDx + pScrollX;
+	this.dy = pDy;
 	this.position.x = (Math.random()*2000) + 1000;
   this.position.y = (Math.random() * (GAME_CONSTANTS.groundHeight - 30) ) + 30;
 }
@@ -14,6 +18,7 @@ Bullet.prototype = Object.create(PIXI.Sprite.prototype);
 
 Bullet.prototype.update = function(pDt) {
 	this.position.x += this.dx * pDt;
+	this.position.y += this.dy * pDt;
 	if(this.position.x < -30){
 		this.respawn();
 	}
